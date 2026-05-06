e23fc71a0ef75b03afa16275fe9507a9483dff59
DanielHassan-dev <danzzy051@gmail.com>
2026-05-06 14:41:50 +0100

    update portfolio

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'your-local-dev-key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
+# DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 
 # ALLOWED_HOSTS = []
